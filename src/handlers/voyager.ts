fbbb2f761cc86be33a7d72d56688b587eb328fa9
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-04-18 15:54:10 +0100

    setup voyagerMiddleware function to add authorisation headers to the request

--- src/handlers/voyager.ts
+++ src/handlers/voyager.ts
@@ -6,8 +6,23 @@ import { projectRoot } from 'src/utils';
 
 const voyagerPath = 'node_modules/graphql-voyager/dist/voyager.worker.js';
 
+const getAuthorizationHeader = () => {
+  const token = process.env.AUTH_TOKEN || '';
+
+  return JSON.stringify({
+    Authorization: `Bearer ${token}`,
+  });
+};
+
 export const voyagerWorkerMiddleware = () => {
   return express.static(path.join(projectRoot, voyagerPath));
 };
-export const voyagerMiddleware = () =>
-  graphqlVoyagerMiddleware({ endpointUrl: '/graphql' });
+export const voyagerMiddleware = () => {
+  return graphqlVoyagerMiddleware({
+    endpointUrl: '/graphql',
+    headersJS: getAuthorizationHeader(),
+    displayOptions: {
+      headerEditorEnabled: true,
+    },
+  });
+};
