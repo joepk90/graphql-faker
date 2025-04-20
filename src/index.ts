c943a05f241fae15c30f8ee9fbffe4878a0d0dd9
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-04-20 17:24:29 +0100

    setup dotend env vars to load in index.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
-
+import * as dotenv from 'dotenv';
 import { getConfig } from 'src/config';
 import { runServer } from 'src/app';
 
+dotenv.config();
+
 (async () => {
   const options = getConfig();
-
   runServer(options);
 })();
