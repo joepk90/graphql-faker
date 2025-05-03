12fbb1b255eff83a7d39cf306dd39c3fbe8ba59d
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-05-03 16:22:53 +0100

    fix bug in customer headers logic - use Object instead of Headers class to handle headers - comment + TODO added to explain why

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -8,21 +8,32 @@ import {
   customHeadersDirName,
 } from 'src/utils';
 
-const validateHeaders = (headers: Object) => {
-  const emptyHeaders = new Headers();
+// TODO: update logic to use node-fetch Headers class
+
+// WARNNG: This will change the requirements of working with the headers object everywhere.
+// header valies are stored as "Authorization": ["Bearer XXX"]
+// This means unless you use the getter method of the Headers object, an array will be passed to the request
+
+const validatedHeaders = (headers: Object) => {
+  const emptyHeaders = Object.create(null);
   if (!headers || typeof headers !== 'object') {
     return emptyHeaders;
   }
 
-  const result = Object.entries(headers).every(
-    ([key, value]) => typeof key === 'string' && typeof value === 'string',
-  );
+  const validatedHeaders = Object.create(null);
+  Object.entries(headers).forEach(([key, value]) => {
+    if (typeof key !== 'string' || typeof value !== 'string') {
+      console.warn(
+        'Headers not valid, excluded from the request. Header Key: ',
+        key,
+      );
+      return;
+    }
 
-  if (!result) {
-    return emptyHeaders;
-  }
+    validatedHeaders[key] = value;
+  });
 
-  return headers as Headers;
+  return validatedHeaders;
 };
 
 export const getCustomHeaders = (): Headers => {
@@ -34,7 +45,7 @@ export const getCustomHeaders = (): Headers => {
 
   const customerHeadersFilePath = `${projectRoot}/${customHeadersDirName}/${customHeadersFileName}`;
   const headers = getJsonFileIfExists(customerHeadersFilePath);
-  return validateHeaders(headers);
+  return validatedHeaders(headers);
 };
 
 // copy the headers from a request
@@ -45,6 +56,10 @@ export const copyHeadersFromRequest = (
   const proxyHeaders = Object.create(null);
 
   for (const name of headersToCopy) {
+    if (!requestHeaders[name]) {
+      continue;
+    }
+
     proxyHeaders[name] = requestHeaders[name];
   }
 
